661bd22b145894e9dc6a9fee4f6e268b5749f42a
TheDi4nier <diego.pro.villanueva@gmail.com>
2023-02-17 00:03:41 +0100

    Page accueil Resposive ! Menu mobile ok

--- public/Services/MenuMobile.js
+++ public/Services/MenuMobile.js
@@ -1,17 +1,20 @@
 
 const menu = document.getElementById("menu");
+const menucontainer = document.querySelector(".menucontainer");
+
 const burgerimg = document.getElementById("burgermenuimg");
 
 let toggle = true;
 
 menuaction.addEventListener("click", function () {
     menu.classList.toggle("open");
+    menucontainer.classList.toggle("open");
     toggle = !toggle;
     if (toggle) {
-        burgerimg.src = '/public/Images/burger.svg';
+        burgerimg.src = '/Images/burger.svg';
     }
     else {
-        burgerimg.src = '/public/Images/burgeropen.svg';
+        burgerimg.src = '/Images/burgeropen.svg';
     }
 });
 
